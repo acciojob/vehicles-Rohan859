13176d2b4584d8574e5298c462d4c1cb2b8dfc4f
Rohan859 <rohankundu859@gmail.com>
2024-01-27 00:45:59 +0530

    all done

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -2,12 +2,17 @@ package com.driver;
 
 public class F1 extends Car {
 
-    public F1(String name, boolean isManual) {
+    public F1(String name, boolean isManual)
+    {
         //Use arbitrary values for parameters which are not mentioned
+        super(name,isManual);
     }
 
-    public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
+    public void accelerate(int rate)
+    {
+        this.setCurrentSpeed(this.getCurrentSpeed()+rate);
+        int newSpeed = this.getCurrentSpeed(); //set the value of new speed by using currentSpeed and rate
+
         /**
          * speed 0: gear 1
          * speed 1-50: gear 1
@@ -17,14 +22,39 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-
-        if(newSpeed == 0) {
-            //Stop the car, set gear as 1
-        }
         //for all other cases, change the gear accordingly
 
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+
+
+
+        if(newSpeed == 0)
+        {
+            //Stop the car, set gear as 1
+            this.setGears(1);
+        }
+        else if(newSpeed>=1 && newSpeed<=50)
+        {
+            this.setGears(1);
+        }
+        else if(newSpeed>=51 && newSpeed<=100)
+        {
+            this.setGears(2);
+        }
+        else if(newSpeed>=101 && newSpeed<=150)
+        {
+            this.setGears(3);
+        }
+        else if(newSpeed>=151 && newSpeed<=200)
+        {
+            this.setGears(4);
+        }
+        else if(newSpeed>=201 && newSpeed<=250)
+        {
+            this.setGears(5);
+        }
+        else
+        {
+            this.setGears(6);
         }
     }
 }
